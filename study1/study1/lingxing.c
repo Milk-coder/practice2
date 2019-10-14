a5402a14b28e5b9ca0ce596871368f6621556564
111 <mcp19971118@163.com>
2019-10-14 17:59:50 +0800

    入门级代码

--- study1/study1/lingxing.c
+++ study1/study1/lingxing.c
@@ -1,28 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main(){
-	int line = 0;
-	int i = 0;
-	scanf("%d", &line);
-	for (i = 0; i < line; i++){
-		int  j = 0;
-		for (j = 0; j < line - 1 - i; j++){
-			printf(" ");
-		}
-		for (j = 0; j < 2 * i + 1; j++){
-			printf("*");
-		}
-		printf("\n");
-	}
-	for (i = 0; i < line - 1; i++){
-		int k = 0;
-		for (k = 0; k < i + 1; k++){
-			printf(" ");
-		}
-		for (k = 0; k < 2*(line-1-i)-1 ; k++){
-			printf("*");
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main(){
+//	int line = 0;
+//	int i = 0;
+//	scanf("%d", &line);
+//	for (i = 0; i < line; i++){
+//		int  j = 0;
+//		for (j = 0; j < line - 1 - i; j++){
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * i + 1; j++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < line - 1; i++){
+//		int k = 0;
+//		for (k = 0; k < i + 1; k++){
+//			printf(" ");
+//		}
+//		for (k = 0; k < 2*(line-1-i)-1 ; k++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
