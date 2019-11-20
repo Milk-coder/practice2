7fd5347cc80cd4ea67437549f90e4546923ab225
Milk-coder <mcp971017@163.com>
2019-11-20 23:30:41 +0800

    练习

---
+++ homework10/homework10/test.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//int main(){
+//	int i;
+//	double sum = 0.0;
+//	for (i = 1; i <= 100; i++){
+//		if (i % 2 == 1){
+//			sum += 1.0 / i;
+//		}
+//		else if  (i % 2 == 0){
+//			sum -= 1.0 / i;
+//		}
+//	}
+//	printf("½á¹ûÎª£º%lf ", sum);
+//	return 0;
+//}
