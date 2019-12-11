991f90f306cb10bd96fe5862079f0472076f0098
Milk-coder <mcp971017@163.com>
2019-12-11 20:22:33 +0800

    练习

---
+++ parctice/src/Soultion.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution665{
+    public boolean  checkPossibility(int[] nums){
+        List<Integer> down = new ArrayList<>();
+        for(int i = 0;i<nums.length -1;i++){
+            if(nums[i] > nums[i+1]){
+
+            }
+        }
+
+    }
+
+}
